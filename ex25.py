d6f7ea8f02506945addf91bfca7429e325b4af51
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-10 22:18:58 +0630

    My 26th python exercise

--- ex25.py
+++ ex25.py
@@ -1,13 +1,13 @@
 def break_words(stuff):
     """This function will break up words for us."""
-    words=stuff.split('')
+    words=stuff.split(' ')
     return words
 def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 def print_first_word(words):
     """Prints the first word after popping it off."""
-    word= words.pop()
+    word= words.pop(0)
     print(word)
 def print_last_word(words):
     """Prints the last word after popping it off."""
