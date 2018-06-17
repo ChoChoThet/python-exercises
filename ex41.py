ba63b47831ff631771dfa8a5b6b3341a23e7779b
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-17 23:55:06 +0630

    My 50th python exercise

--- ex41.py
+++ ex41.py
@@ -1,11 +1,15 @@
 import random
 from urllib.request import urlopen
 import sys
+
 WORD_URL="http://learncdethehardway.org/words.txt"
+
 WORDS=[]
-PHRASES={"class %%%(%%%):":
-           "Make a class named %%% that is-a %%%.",
-           "class %%%(object):\n\tdef __int__(self,***)":
+
+PHRASES={
+       "class %%%(%%%):":
+       "Make a class named %%% that is-a %%%.",
+       "class %%%(object):\n\tdef __int__(self,***)":
            "class %%% has-a __init__that takes self and ***params.",
            "class %%%(object):\n\tdef ***(self,@@@)":
            "class %%% has-a function *** that takes iself and @@@params.",
---
+++ ex51app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+app = Flask(_name_)
+@app.route("/hello", methods=['POST', 'GET'])
+def index():
+    greeting = "Hello World"
+    if request.method == "POST":
+        name = request.form['name']
+        greet = request.form['greet']
+        greeting = f"{greet},{name}"
+        return render_template("index.html", greeting=greeting)
+    else:
+        return render_template("hello_form.html")
+if _name_ == "_main_":
+    app.run()
