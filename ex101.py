99108f681e61cadce5c21a6ae08e9557a0a913c1
ChoChoThet <moetharkaungkincho@gmail.com>
2018-06-26 12:51:38 +0630

    Solve merge conflict

--- ex101.py
+++ ex101.py
@@ -1,5 +1,5 @@
 tabby_cat= "\t I'm tabbed in ."
-persian_ cat="I'm split \n on a line"
+persian_cat="I'm split \n on a line"
 backslash_cat = "I'm \\a  \\ cat;"
 
 fat_cat = """ I'll do a list: 
@@ -9,12 +9,18 @@ fat_cat = """ I'll do a list:
             """
 
 
-
+tabby_rat="\t I'm tabbed in."
+persian_rat="I'm split \n on a line"
+backslash_rat = "I'm \\ a \\ cat:"
+fat_rat = """ I'll do a list:
+    \t* rat food
+    \t* fishes
+    \t* catnip \n\t * Grass
+    """
 print(tabby_rat)
 print(persian_rat)
 print(backslash_rat)
 print(fat_rat)
-`
 
 print(tabby_cat)
 print(persian_cat)
